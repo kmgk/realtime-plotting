0d1a376460746d8ba1faddb52e426f24b52b0a87
kmgk <48932866+kmgk@users.noreply.github.com>
2024-12-11 12:46:43 +0900

    "Commit Log"

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 url = "https://raw.githubusercontent.com/kmgk/realtime-plotting/refs/heads/main/02_CBA9_flowpath5_sample1_Tscan_heating_9T_probe1mA.txt"
 df = pd.read_csv(url, sep='\t')
-st.title('streamlit Tutorial')
-input_num = st.number_input('Input a number', value=0)
+st.title('リアルタイムプロット')
 
 result = input_num ** 2
 st.write('Result: ', result)
