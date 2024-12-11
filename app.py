ccdb81cb6226109ab73c96fc335dcd6f26c7f3a5
kmgk <48932866+kmgk@users.noreply.github.com>
2024-12-11 13:34:20 +0900

    "Commit Log"

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
+import datetime
+
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+now = datetime.datetime.now(JST)
 
 url = "https://raw.githubusercontent.com/kmgk/realtime-plotting/refs/heads/main/02_CBA9_flowpath5_sample1_Tscan_heating_9T_probe1mA.txt"
 df = pd.read_csv(url, sep='\t')
-st.title('リアルタイムプロット')
 
+st.title('リアルタイムプロット')
+st.write(now.strftime('%H:%M:%S'))
 st.scatter_chart(df, x=" _temp_A ", y=" _diff ")
 st.scatter_chart(df, x=' _time ', y=" _temp_A ")
 st.scatter_chart(df, x=" _time ", y=" _diff ")
