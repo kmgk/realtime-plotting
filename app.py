68b7c32edd7ad0c3248e3ade6f31e654366392e8
kmgk <48932866+kmgk@users.noreply.github.com>
2024-12-11 12:36:22 +0900

    ho

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
+
+df = pd.read_csv("02_CBA9_flowpath5_sample1_Tscan_heating_9T_probe1mA.txt", sep='\t')
 st.title('streamlit Tutorial')
 input_num = st.number_input('Input a number', value=0)
 
 result = input_num ** 2
 st.write('Result: ', result)
+
+st.line_chart(df.filter(items=[' _amp ']))
+st.scatter_chart(df, x=" _temp_A ", y=" _diff ")
